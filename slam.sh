70b06b69a135b7b994840fe35dc1b29f1e5162a3
kepler <githubkepler.50s@gishpuppy.com>
2016-02-16 11:19:07 +0530

    added --count --header

--- slam.sh
+++ slam.sh
@@ -14,18 +14,69 @@
 #        AUTHOR: YOUR NAME (), 
 #  ORGANIZATION: 
 #       CREATED: 02/11/2016 12:38
-#      REVISION:  2016-02-14 20:41
+#      REVISION:  2016-02-16 11:12
 #===============================================================================
 
 file=all.tsv
+DELIM=$'\t'
+
+OPT_VERBOSE=
+OPT_DEBUG=
+while [[ $1 = -* ]]; do
+    case "$1" in
+        -V|--verbose)   shift
+            OPT_VERBOSE=1
+            ;;
+        -H|--header)   shift
+            OPT_HEADER=1
+            ;;
+        -c|--count)   shift
+            OPT_COUNT=1
+            ;;
+        --debug)        shift
+            OPT_DEBUG=1
+            ;;
+        -h|--help)
+            cat <<-! | sed 's|^     ||g'
+            $0 Version: 1.0.0 Copyright (C) 2016 senti
+            This program prints slam information using arguments as filtering criteria.
+            It prints rows that match all criteria.
+
+            Usage:
+            $0 FO  Evert
+
+            Options:
+            -H, --header      Print header (for csvlook)
+            -c, --count       Print rowcount at end
+            -V, --verbose     Displays more information
+                --debug       Displays debug information
+!
+            # no shifting needed here, we'll quit!
+            exit
+            ;;
+        --edit)
+            echo "this is to edit the file generated if any "
+            exit
+            ;;
+        --source)
+            echo "this is to edit the source "
+            vim $0
+            exit
+            ;;
+        *)
+            echo "Error: Unknown option: $1" >&2   
+            echo "Use -h or --help for usage" 1>&2
+            exit 1
+            ;;
+    esac
+done
 
 if [  $# -eq 0 ]; then
     echo -e "Please pass one or more search terms such as 2009 AO/WO/USO or Name of player" 1<&2
     exit 1
 fi
 if [[ "$1" = "wta" ]]; then
-    # ln this file from wta dir
-    file="wtaslam.tsv"
+    file="wta/wtaslam.tsv"
     shift
 fi
 
@@ -39,4 +90,10 @@ for var in "$@"
 do
     text=$(echo "$text" | grep "$var")
 done
+if [[ -n "$OPT_HEADER" ]]; then
+    echo "Event${DELIM}Year${DELIM}Country${DELIM}Winner${DELIM}Country${DELIM}Runner-up${DELIM}Score"
+fi
 echo -e "$text" 
+if [[ -n "$OPT_COUNT" ]]; then
+    echo -e "$text" | wc -l
+fi
